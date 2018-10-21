f540e5c859a21b37ac387dc19608b550d3abfe20
Sebastian Carabali <sebastiancc@unicauca.edu.co>
2018-10-21 00:47:25 -0500

    LOL

--- Board.py
+++ Board.py
@@ -9,36 +9,40 @@ Created on Tue Sep 25 09:31:26 2018
 
 from tkinter import *
 from reversy import *
+from tkinter import messagebox
+
 
 class App:
-       
+
     def __init__(self, master, game):
         self.frame = Frame(master)
         self.frame.pack()
-        self.height=600
-        self.width=600
-        self.grid_column=8
-        self.grid_row=8
-        self.game = game # Reversi Game
+        self.height = 600
+        self.width = 600
+        self.grid_column = 8
+        self.grid_row = 8
+        self.game = game  # Reversi Game
         self.canvas = Canvas(self.frame, height=self.height, width=self.width)
         self.cellwidth = int(self.canvas["width"])/self.grid_column
         self.cellheight = int(self.canvas["height"])/self.grid_row
         self.draw_grid()
         self.canvas.pack()
-        self.pos=(0,0)
+        self.pos = (0, 0)
+        self.sv = StringVar()
+        self.sv.set("white: 2 ===== black: 2")
         self.drawChips()
-        
-        
+        self.lbl_score = Label(self.frame, textvariable=self.sv)
+
         def handler(event, self=self):
             return self.__onClick(event)
-        
+
         self.canvas.bind('<Button-1>', handler)
-        
-        
-        self.hi_there = Button(self.frame, text="Jugar", command=self.start_Game)
+
+        self.hi_there = Button(self.frame, text="Jugar",
+                               command=self.start_Game)
         self.hi_there.pack(side=LEFT)
-        
-    
+        self.lbl_score.pack(side=RIGHT)
+
     def draw_grid(self):
         for i in range(self.grid_row):
             for j in range(self.grid_column):
@@ -47,47 +51,75 @@ class App:
                 x2 = x1 + self.cellwidth
                 y2 = y1 + self.cellheight
                 self.canvas.create_rectangle(x1, y1, x2, y2, fill="green")
-        
-        
+
     def drawChip(self):
-        x=self.pos[1]*self.cellwidth
-        y=self.pos[0]*self.cellheight
+        x = self.pos[1]*self.cellwidth
+        y = self.pos[0]*self.cellheight
         if(self.game.current_player == HUMAN_PLAYER):
-            self.canvas.create_oval(x,y,x+self.cellwidth,y+self.cellheight, fill='black')
+            self.canvas.create_oval(
+                x, y, x+self.cellwidth, y+self.cellheight, fill='black')
         else:
-            self.canvas.create_oval(x,y,x+self.cellwidth,y+self.cellheight, fill='white')
-            
-    
+            self.canvas.create_oval(
+                x, y, x+self.cellwidth, y+self.cellheight, fill='white')
+
     def drawChips(self):
         for i in range(8):
             for j in range(8):
-                val=self.game.board[i][j]
+                val = self.game.board[i][j]
                 print
-                x=j*self.cellwidth
-                y=i*self.cellheight
+                x = j*self.cellwidth
+                y = i*self.cellheight
                 if(val == HUMAN_PLAYER):
-                    self.canvas.create_oval(x,y,x+self.cellwidth,y+self.cellheight, fill='black')
+                    self.canvas.create_oval(
+                        x, y, x+self.cellwidth, y+self.cellheight, fill='black')
                 elif(val == AI_PLAYER):
-                    self.canvas.create_oval(x,y,x+self.cellwidth,y+self.cellheight, fill='white')
-            
+                    self.canvas.create_oval(
+                        x, y, x+self.cellwidth, y+self.cellheight, fill='white')
+
     def __onClick(self, event):
-        
-            i=int(event.y/self.cellheight)
-            j=int(event.x/self.cellwidth)
-            self.pos =(i,j)
-            if self.game.make_move(self.game.board, self.game.current_player, i , j):
-                self.game.change_player()
+        if self.game.check_for_winner:
+            sn, sb = self.game.get_board_score(self.game.board)
+            messagebox.showinfo("Info", "Juego terminado, puntajes humano = {}, ai = {}.".format(
+                sn, sb
+            ))
+            if sb > sn:
+                messagebox.showinfo("Info", "Ganan ai")
+            elif sb < sn:
+                messagebox.showinfo("Info", "Ganan humano")
             else:
-                print("Error")
-            
-            self.drawChips()
-        
-       
-    
-    def  start_Game(self):
+                messagebox.showinfo("Info", "Empate")
+            return None
+
+        i = int(event.y/self.cellheight)
+        j = int(event.x/self.cellwidth)
+        self.pos = (i, j)
+        if self.game.no_moves and self.game.has_to_yield_turn(self.game.board, HUMAN_PLAYER):
+            messagebox.showinfo(
+                "Info", "Juego terminado, no hay movimientos válidos.")
+            return None
+        if self.game.current_player == HUMAN_PLAYER:
+            if self.game.has_to_yield_turn(self.game.board, HUMAN_PLAYER):
+                self.game.no_moves = True
+                self.game.change_player()
+                self.game.make_computer_move(self.game.board)
+                return None
+            elif self.game.make_move(self.game.board, self.game.current_player, i, j):
+                self.game.no_moves = False
+                self.game.change_player()
+                self.game.make_computer_move(self.game.board)
+            else:
+                messagebox.showerror("Error", "Movimiento invalido.")
+        score = self.game.get_board_score(self.game.board)
+        self.sv.set("white: {} ===== black: {}".format(
+            score[1],
+            score[0]
+        ))
+        self.drawChips()
+
+    def start_Game(self):
         print('start game')
-        
-        
+
+
 r = Reversi()
 root = Tk()
 app = App(root, r)
--- reversy.py
+++ reversy.py
@@ -8,12 +8,15 @@
 AI_PLAYER = 1 # blancas
 HUMAN_PLAYER = -1 # negros
 EMPTY = 0
+DEPTH = 2
 
 class Reversi:
 
     def __init__(self):
         self.board = self.new_board()
         self.current_player = HUMAN_PLAYER
+        self.no_moves = False
+        self.check_for_winner = False
         self.init_board()
 
     def new_board(self):
@@ -75,6 +78,8 @@ class Reversi:
             board[xi][yi] = player
             for x, y in to_change:
                 board[x][y] = player
+            if self.is_full_board(board):
+                self.check_for_winner = True
             return True
         else:
             return False
@@ -98,4 +103,110 @@ class Reversi:
     
     def change_player(self):
         self.current_player = AI_PLAYER if self.current_player == HUMAN_PLAYER else HUMAN_PLAYER
+
+    def actions(self, state, player):
+        actions = []
+        for x in range(8):
+            for y in range(8):
+                if len(self.is_valid(state, player, x, y)) > 0:
+                    actions.append([x, y])
+        return actions
+    
+    def make_copy(self, state):
+        new_state = self.new_board()
+        for x in range(8):
+            for y in range(8):
+                new_state[x][y] = state[x][y]
+        return new_state
+
+    def make_computer_move(self, state):
+        if self.has_to_yield_turn(state, AI_PLAYER):
+            self.change_player()
+            self.no_moves = True
+            return None
+        else:
+            move = self.minimax_decision(state, self.current_player)
+            x, y = move
+            self.no_moves = False
+            self.make_move(state, self.current_player, x, y)
+            self.change_player()
+
+    def minimax_decision(self, state, player):
+        other_player = AI_PLAYER if player == HUMAN_PLAYER else HUMAN_PLAYER
+        move_list = self.actions(state, player)
+
+        if len(move_list) == 0:
+            return [-1, -1]
+        else:
+            best_score = -float("inf")
+            best_move = [-1,-1]
+            
+            for x, y in move_list:
+                state_copy = self.make_copy(state)
+                self.make_move(state_copy, player, x, y)
+                val = self.minimax_value(state_copy, player, other_player, 1)
+
+                if val > best_score:
+                    best_score = val
+                    best_move = [x, y]
+            
+            return best_move
+
+    def is_full_board(self, state):
+        for x in range(8):
+            for y in range(8):
+                if state[x][y] == EMPTY:
+                    return False
+        return True
+
+    def minimax_value(self, state, original_player, current_player, search_ply):
+        if search_ply == DEPTH or self.is_full_board(state):
+            return self.score(state, original_player)
+        
+        other_player = AI_PLAYER if current_player == HUMAN_PLAYER else HUMAN_PLAYER
+        
+        move_list = self.actions(state, current_player)
+
+        if len(move_list) == 0:
+            return self.minimax_value(state, original_player, other_player, search_ply + 1)
+        else:
+            best_score = -float("inf")
+            if original_player != current_player:
+                best_score = float("inf")
+            
+            for x, y in move_list:
+                copy_state = self.make_copy(state)
+                self.make_move(copy_state, current_player, x, y)
+                val = self.minimax_value(copy_state, original_player, other_player, search_ply + 1)
+
+                if original_player == current_player:
+                    if val > best_score:
+                        best_score = val
+                else:
+                    if val < best_score:
+                        best_score = val
+            return best_score
+
+    def is_game_over(self):
+
+        pass
+
+    def has_to_yield_turn(self, state, player):
+        has_to_yield = True
+        possible_moves = self.actions(state, player)
+        if len(possible_moves) > 0:
+            has_to_yield = False
+        return has_to_yield
+    
+    def get_board_score(self, state):
+        bscore = 0
+        wscore = 0
+
+        for x in range(8):
+            for y in range(8):
+                if state[x][y] == AI_PLAYER:
+                    wscore += 1
+                elif state[x][y] == HUMAN_PLAYER:
+                    bscore += 1
+        return (bscore, wscore)
 
